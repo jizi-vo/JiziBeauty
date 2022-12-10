f2d456b42c64575d18e07ed4eac01b6d489db8d7
jizi vo <votranhuyentran1999@gmail.com>
2022-12-11 02:11:18 +0700

    cs

--- resources/views/admin_layout.blade.php
+++ resources/views/admin_layout.blade.php
@@ -51,7 +51,7 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
     <!--search & user info start-->
     <ul class="nav pull-right top-menu">
         <li>
-            <input type="text" class="form-control search" placeholder=" Search">
+           
         </li>
         <!-- user login dropdown start-->
         <li class="dropdown">
@@ -68,8 +68,6 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
                 <b class="caret"></b>
             </a>
             <ul class="dropdown-menu extended logout">
-                <li><a href="#"><i class=" fa fa-suitcase"></i>Profile</a></li>
-                <li><a href="#"><i class="fa fa-cog"></i> Settings</a></li>
                 <li><a href="{{URL::to('/logout')}}"><i class="fa fa-key"></i> Log Out</a></li>
             </ul>
         </li>
